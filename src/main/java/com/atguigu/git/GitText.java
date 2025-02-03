787783ea6f2d902cd493ae448da79dbadbfe3bcd
mengxiang <3176033626@qq.com>
2025-02-03 15:12:20 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitText.java
+++ src/main/java/com/atguigu/git/GitText.java
@@ -5,5 +5,6 @@ public class GitText {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
+        System.out.println("Hello Git4");
     }
 }
