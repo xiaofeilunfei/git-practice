df73226036bff141bed3798faa2b335a66ad0b7c
mengxiang <3176033626@qq.com>
2025-02-03 15:03:56 +0800

    second commit

--- src/main/java/com/atguigu/git/GitText.java
+++ src/main/java/com/atguigu/git/GitText.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class GitText {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
     }
 }
